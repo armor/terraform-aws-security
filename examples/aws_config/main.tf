107d3380c7ec40ad102c008e9e9deaf02451b502
Gerard Setho <gerard.setho@quantum.security>
2021-07-07 15:16:41 +0800

    EN-469: Add example code (feat)

---
+++ examples/aws_config/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.12.26"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "example_aws_config" {
+  source            = "../../modules/aws-config"
+  name              = var.name_prefix
+  enable_aws_config = var.enable_aws_config
+}
