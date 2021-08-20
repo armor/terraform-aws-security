92928d1e5995d5cd0af3c154dd4c8cebc903f234
Gerard Setho <gerard.setho@quantum.security>
2021-08-20 13:46:17 +0800

    EN-469: Add rule: S3_BUCKET_PUBLIC_READ_PROHIBITED (patch)

--- modules/aws-config/main.tf
+++ modules/aws-config/main.tf
@@ -179,6 +179,19 @@ resource "aws_config_config_rule" "s3_account_level_public_access_blocks" {
   depends_on = [aws_config_configuration_recorder.recorder]
 }
 
+resource "aws_config_config_rule" "s3_bucket_public_read_prohibited" {
+  name = "s3_bucket_public_read_prohibited"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
+  }
+
+  maximum_execution_frequency = null
+
+  depends_on = [aws_config_configuration_recorder.recorder]
+}
+
 resource "aws_config_config_rule" "iam_password_policy" {
   name = "iam_password_policy"
 
